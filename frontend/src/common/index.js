23a44e0a96b3d5b4a61cf1fcd894198c95fbe5a0
vuongminh042 <vuongchiminh320@gmail.com>
2024-09-25 23:29:22 +0700

    update code

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,81 +1,81 @@
 const backendDomin = "http://localhost:8080"
 
 const SummaryApi = {
-    signUP : {
-        url : `${backendDomin}/api/signup`,
-        method : "post"
+    signUP: {
+        url: `${backendDomin}/api/signup`,
+        method: "post"
     },
-    signIn : {
-        url : `${backendDomin}/api/signin`,
-        method : "post"
+    signIn: {
+        url: `${backendDomin}/api/signin`,
+        method: "post"
     },
-    current_user : {
-        url : `${backendDomin}/api/user-details`,
-        method : "get"
+    current_user: {
+        url: `${backendDomin}/api/user-details`,
+        method: "get"
     },
-    logout_user : {
-        url : `${backendDomin}/api/userLogout`,
-        method : 'get'
+    logout_user: {
+        url: `${backendDomin}/api/userLogout`,
+        method: 'get'
     },
-    allUser : {
-        url : `${backendDomin}/api/all-user`,
-        method : 'get'
+    allUser: {
+        url: `${backendDomin}/api/all-user`,
+        method: 'get'
     },
-    updateUser : {
-        url : `${backendDomin}/api/update-user`,
-        method : "post"
+    updateUser: {
+        url: `${backendDomin}/api/update-user`,
+        method: "post"
     },
-    uploadProduct : {
-        url : `${backendDomin}/api/upload-product`,
-        method : 'post'
+    uploadProduct: {
+        url: `${backendDomin}/api/upload-product`,
+        method: 'post'
     },
-    allProduct : {
-        url : `${backendDomin}/api/get-product`,
-        method : 'get'
+    allProduct: {
+        url: `${backendDomin}/api/get-product`,
+        method: 'get'
     },
-    updateProduct : {
-        url : `${backendDomin}/api/update-product`,
-        method  : 'post'
+    updateProduct: {
+        url: `${backendDomin}/api/update-product`,
+        method: 'post'
     },
-    categoryProduct : {
-        url : `${backendDomin}/api/get-categoryProduct`,
-        method : 'get'
+    categoryProduct: {
+        url: `${backendDomin}/api/get-categoryProduct`,
+        method: 'get'
     },
-    categoryWiseProduct : {
-        url : `${backendDomin}/api/category-product`,
-        method : 'post'
+    categoryWiseProduct: {
+        url: `${backendDomin}/api/category-product`,
+        method: 'post'
     },
-    productDetails : {
-        url : `${backendDomin}/api/product-details`,
-        method : 'post'
+    productDetails: {
+        url: `${backendDomin}/api/product-details`,
+        method: 'post'
     },
-    addToCartProduct : {
-        url : `${backendDomin}/api/addtocart`,
-        method : 'post'
+    addToCartProduct: {
+        url: `${backendDomin}/api/addtocart`,
+        method: 'post'
     },
-    addToCartProductCount : {
-        url : `${backendDomin}/api/countAddToCartProduct`,
-        method : 'get'
+    addToCartProductCount: {
+        url: `${backendDomin}/api/countAddToCartProduct`,
+        method: 'get'
     },
-    addToCartProductView : {
-        url : `${backendDomin}/api/view-card-product`,
-        method : 'get'
+    addToCartProductView: {
+        url: `${backendDomin}/api/view-card-product`,
+        method: 'get'
     },
-    updateCartProduct : {
-        url : `${backendDomin}/api/update-cart-product`,
-        method : 'post'
+    updateCartProduct: {
+        url: `${backendDomin}/api/update-cart-product`,
+        method: 'post'
     },
-    deleteCartProduct : {
-        url : `${backendDomin}/api/delete-cart-product`,
-        method : 'post'
+    deleteCartProduct: {
+        url: `${backendDomin}/api/delete-cart-product`,
+        method: 'post'
     },
-    searchProduct : {
-        url : `${backendDomin}/api/search`,
-        method : 'get'
+    searchProduct: {
+        url: `${backendDomin}/api/search`,
+        method: 'get'
     },
-    filterProduct : {
-        url : `${backendDomin}/api/filter-product`,
-        method : 'post'
+    filterProduct: {
+        url: `${backendDomin}/api/filter-product`,
+        method: 'post'
     }
 }
 
